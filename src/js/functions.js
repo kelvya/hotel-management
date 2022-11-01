a8323bbddab71a6b9229d9c2f83c9b360b5fb9e9
Andrey Mendes <andreylsm@gmail.com>
2022-11-01 09:23:24 -0300

    Incremento funções Javascript

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,43 +1,46 @@
-let formulario = document.querySelector("#login_formulario");
-let usuario = 'joaozinho_silva@gmail.com';
-let senha = '123456';
-
+//---------------------------------------------------------------------
+// LOGIN
+//---------------------------------------------------------------------
+const formularioLogin = document.querySelector("#login_formulario")
 
 // Ao clicar no botão CONFIRMAR, o formulário é submetido
-formulario.addEventListener('submit', (e) => {
-    // console.log(formulario.elements["usuarioLogin"].value);
-    // console.log(formulario.elements["senhaLogin"].value.length)
+formularioLogin.addEventListener("submit", (e) => {
+    let nome = "João Silva"
+    let usuario = "joaozinho_silva@gmail.com"
+    let senha = "123456"
 
     // Remove caracteres do campo de usuário
-    let caracteresRemoviveis = ['"', "'", ' '];
-    let usuarioLimpo = formulario.elements["usuarioLogin"].value;
+    let caracteresRemoviveis = ['"', "'", " "]
+    let usuarioLimpo = formularioLogin.elements["usuarioLogin"].value
+
     // Verifica letra a letra se existe algum dos caracteres removíveis e caso tenha, substitui por espaço em branco
     for (let pos of caracteresRemoviveis) {
-        usuarioLimpo = usuarioLimpo.split(pos).join("");
+        usuarioLimpo = usuarioLimpo.split(pos).join("")
     }
 
     // Verificar se os valores dos campos são maiores que 1 (possuem algum caracter)
     if (
-        formulario.elements["usuarioLogin"].value.length < 1 ||
-        formulario.elements["senhaLogin"].value.length < 1
+        formularioLogin.elements["usuarioLogin"].value.length < 1 ||
+        formularioLogin.elements["senhaLogin"].value.length < 1
     ) {
         alert("Favor preencher usuário e senha")
-        // e.preventDefault()
-        return;
+        // Pausa o 'submit'
+        e.preventDefault()
+        return
     }
 
     // Verificar se o usuário e senha conferem com o usuário criado
     if (
-        formulario.elements["usuarioLogin"].value !== usuario ||
-        formulario.elements["senhaLogin"].value !== senha
+        formularioLogin.elements["usuarioLogin"].value !== usuario ||
+        formularioLogin.elements["senhaLogin"].value !== senha
     ) {
         alert("Usuário ou senha não encontrado")
-        // e.preventDefault()
-        return;
+        e.preventDefault()
+        return
     }
 
-    localStorage.setItem("usuario", formulario.elements["usuarioLogin"].value);
-
-    // // Pausa o 'submit'
-    // e.preventDefault()
-});
+    localStorage.setItem(
+        "usuario",
+        formularioLogin.elements["usuarioLogin"].value
+    )
+})
