f32ecffffa8d9e0be1345720a61781dc420f05b4
Andrey Mendes <andreylsm@gmail.com>
2022-11-16 20:37:02 -0300

    Atualização reservas js v.2

--- src/js/reservas.js
+++ src/js/reservas.js
@@ -20,77 +20,66 @@ const quartos = [
 const checkin = document.querySelector("#checkin")
 const checkout = document.querySelector("#checkout")
 const qtdPessoas = document.querySelector("#qtdPessoas")
-const resumo = document.querySelector("#bookingSummary p")
 
 let qtdAtual = 1
 
 // Carrega informações do localstorage (caso já exista)
 localStorage.getItem("checkin")
-    ? (resumo.children[1].innerText += localStorage.getItem("checkin"))
+    ? document.querySelector("#bookingCheckIn").innerText = localStorage.getItem("checkin")
     : null
 
 localStorage.getItem("checkout")
-    ? (resumo.children[2].innerText += localStorage.getItem("checkout"))
+    ? (document.querySelector("#bookingCheckOut").innerText = localStorage.getItem("checkout"))
     : null
 
 if(localStorage.getItem("qtdPessoas")){
     qtdAtual = localStorage.getItem("qtdPessoas")
 }
-resumo.children[3].innerText += qtdAtual
+document.querySelector("#bookingQtd").innerText = qtdAtual
 
 if (localStorage.getItem("quarto")) {
     switch (localStorage.getItem("quarto")) {
         case "classic":
-            resumo.children[0].innerText += "Classic"
-            resumo.children[4].innerText += (quartos[1]["valor"] * qtdAtual)
+            document.querySelector("#bookingApt").innerText = "Classic"
+            document.querySelector("#bookingValor").innerText = quartos[1]["valor"] * qtdAtual
             break
         case "premium":
-            resumo.children[0].innerText += "Premium"
-            resumo.children[4].innerText += (quartos[2]["valor"] * qtdAtual)
+            document.querySelector("#bookingApt").innerText = "Premium"
+            document.querySelector("#bookingValor").innerText += (quartos[2]["valor"] * qtdAtual)
             break
         default:
-            resumo.children[0].innerText += "Executive"
-            resumo.children[4].innerText += (quartos[0]["valor"] * qtdAtual)
+            document.querySelector("#bookingApt").innerText = "Executive"
+            document.querySelector("#bookingValor").innerText += (quartos[0]["valor"] * qtdAtual)
             break
     }
 }
 
-
-
 // Guarda no localstorage o valor do checkin sempre que for mudado
 checkin.onchange = function() {
     localStorage.setItem("checkin", checkin.value)
-    resumo.children[1].innerText = `Check-in: ${localStorage.getItem("checkin")}`
+    document.querySelector("#bookingCheckIn").innerText = `${localStorage.getItem("checkin")}`
 }
 // Guarda no localstorage o valor do checkout sempre que for mudado
 checkout.onchange = function () {
     localStorage.setItem("checkout", checkout.value)
-    resumo.children[2].innerText = `Check-out: ${localStorage.getItem(
-        "checkout"
-    )}`
+    document.querySelector("#bookingCheckOut").innerText = `${localStorage.getItem("checkout")}`
 
 }
 // Guarda no localstorage o valor da quantidade de pessoas sempre que for mudado
 qtdPessoas.onchange = function () {
     localStorage.setItem("qtdPessoas", qtdPessoas.value)
     qtdAtual = localStorage.getItem("qtdPessoas")
-    resumo.children[3].innerText = `Pessoas: ${qtdAtual}`
+    document.querySelector("#bookingQtd").innerText = `${qtdAtual}`
 
-    switch (resumo.children[0].innerText) {
-        case "Apartamento: Classic":
-            resumo.children[4].innerText = `Valor total: ${
-                quartos[1]["valor"] * qtdAtual
-            }`
+    switch (document.querySelector("#bookingApt").innerText) {
+        case "Classic":
+            document.querySelector("#bookingValor").innerText = `R$ ${(quartos[1]["valor"] * qtdAtual).toFixed(2)}`
             break
-        case "Apartamento: Premium":
-            resumo.children[4].innerText = `Valor total: ${
-                quartos[2]["valor"] * qtdAtual
-            }`
+        case "Premium":
+            document.querySelector("#bookingValor").innerText = `R$ ${(quartos[2]["valor"] * qtdAtual).toFixed(2)}`
             break
         default:
-            resumo.children[4].innerText = `Valor total: ${
-                quartos[0]["valor"] * qtdAtual
-            }`
+            document.querySelector("#bookingValor").innerText = `R$ ${(quartos[0]["valor"] * qtdAtual).toFixed(2)}`
             break
     }
 }
@@ -103,24 +92,18 @@ $("input[name='roomChoice']").click(function() {
         switch ($(this).val()) {
             case "classic":
                 localStorage.setItem("quarto", quartos[1]['nome'])
-                resumo.children[0].innerText = `Apartamento: Classic`
-                resumo.children[4].innerText = `Valor total: ${
-                    quartos[1]["valor"] * qtdAtual
-                }`
+                document.querySelector("#bookingApt").innerText = `Classic`
+                document.querySelector("#bookingValor").innerText = `R$ ${(quartos[1]["valor"] * qtdAtual).toFixed(2)}`
                 break
             case "premium":
                 localStorage.setItem("quarto", quartos[2]["nome"])
-                resumo.children[0].innerText = `Apartamento: Premium`
-                resumo.children[4].innerText = `Valor total: ${
-                    quartos[2]["valor"] * qtdAtual
-                }`
+                document.querySelector("#bookingApt").innerText = `Premium`
+                document.querySelector("#bookingValor").innerText = `R$ ${(quartos[2]["valor"] * qtdAtual).toFixed(2)}`
                 break
             default:
                 localStorage.setItem("quarto", quartos[0]["nome"])
-                resumo.children[0].innerText = `Apartamento: Executive`
-                resumo.children[4].innerText = `Valor total: ${
-                    quartos[0]["valor"] * qtdAtual
-                }`
+                document.querySelector("#bookingApt").innerText = `Executive`
+                document.querySelector("#bookingValor").innerText = `R$ ${(quartos[0]["valor"] * qtdAtual).toFixed(2)}`
                 break
         }
     }
