6fc711ee82023428e1e840074e922797b55396e7
Andrey Mendes <andreylsm@gmail.com>
2022-11-17 21:50:33 -0300

    Atualização reservas js v.3 - continuar

--- src/js/reservas.js
+++ src/js/reservas.js
@@ -4,15 +4,21 @@
 const quartos = [
     {
         nome: "executive",
-        valor: 190
+        valor: 190,
+        descricao:
+            "Descrição EXECUTIVE Standard ipsum dolor sit amet consectetur adipisicing elit. Sit nobis aliquam quibusdam aliquid quam perferendis autem eum numquam earum nam corporis mollitia cumque error, id quod laboriosam standard.",
     },
     {
         nome: "classic",
-        valor: 250
+        valor: 250,
+        descricao:
+            "Descrição CLASSIC Standard ipsum dolor sit amet consectetur adipisicing elit. Sit nobis aliquam quibusdam aliquid quam perferendis autem eum numquam earum nam corporis mollitia cumque error, id quod laboriosam standard.",
     },
     {
         nome: "premium",
-        valor: 400
+        valor: 400,
+        descricao:
+            "Descrição PREMIUM Standard ipsum dolor sit amet consectetur adipisicing elit. Sit nobis aliquam quibusdam aliquid quam perferendis autem eum numquam earum nam corporis mollitia cumque error, id quod laboriosam standard.",
     }
 ]
 
@@ -24,16 +30,19 @@ const qtdPessoas = document.querySelector("#qtdPessoas")
 let qtdAtual = 1
 
 // Carrega informações do localstorage (caso já exista)
-localStorage.getItem("checkin")
-    ? document.querySelector("#bookingCheckIn").innerText = localStorage.getItem("checkin")
-    : null
+if(localStorage.getItem("checkin")) {
+    document.querySelector("#bookingCheckIn").innerText = localStorage.getItem("checkin")
+    checkin.value = localStorage.getItem("checkin")
+}
 
-localStorage.getItem("checkout")
-    ? (document.querySelector("#bookingCheckOut").innerText = localStorage.getItem("checkout"))
-    : null
+if(localStorage.getItem("checkout")) {
+    document.querySelector("#bookingCheckOut").innerText = localStorage.getItem("checkout")
+    checkout.value = localStorage.getItem("checkout")
+}
 
 if(localStorage.getItem("qtdPessoas")){
     qtdAtual = localStorage.getItem("qtdPessoas")
+    qtdPessoas.value = qtdAtual
 }
 document.querySelector("#bookingQtd").innerText = qtdAtual
 
@@ -41,15 +50,18 @@ if (localStorage.getItem("quarto")) {
     switch (localStorage.getItem("quarto")) {
         case "classic":
             document.querySelector("#bookingApt").innerText = "Classic"
-            document.querySelector("#bookingValor").innerText = quartos[1]["valor"] * qtdAtual
+            document.querySelector("#bookingValor_modal").innerText = `R$ ${(quartos[1]["valor"] * qtdAtual).toFixed(2)}`
+            document.querySelector("#classic").checked = true
             break
         case "premium":
             document.querySelector("#bookingApt").innerText = "Premium"
-            document.querySelector("#bookingValor").innerText += (quartos[2]["valor"] * qtdAtual)
+            document.querySelector("#bookingValor_modal").innerText = `R$ ${(quartos[2]["valor"] * qtdAtual).toFixed(2)}`
+            document.querySelector("#premium").checked = true
             break
         default:
             document.querySelector("#bookingApt").innerText = "Executive"
-            document.querySelector("#bookingValor").innerText += (quartos[0]["valor"] * qtdAtual)
+            document.querySelector("#bookingValor_modal").innerText = `R$ ${(quartos[0]["valor"] * qtdAtual).toFixed(2)}`
+            document.querySelector("#executive").checked = true
             break
     }
 }
@@ -58,12 +70,26 @@ if (localStorage.getItem("quarto")) {
 checkin.onchange = function() {
     localStorage.setItem("checkin", checkin.value)
     document.querySelector("#bookingCheckIn").innerText = `${localStorage.getItem("checkin")}`
+
+    //Apaga valor de checkout
+    checkout.value = ''
+    document.querySelector("#bookingCheckOut").innerText = ''
+    localStorage.removeItem("checkout")
 }
 // Guarda no localstorage o valor do checkout sempre que for mudado
 checkout.onchange = function () {
-    localStorage.setItem("checkout", checkout.value)
-    document.querySelector("#bookingCheckOut").innerText = `${localStorage.getItem("checkout")}`
 
+    //Verifica se checkout é posterior ao check-in
+    if (checkout.value > checkin.value) {
+        localStorage.setItem("checkout", checkout.value)
+        document.querySelector("#bookingCheckOut").innerText = `${localStorage.getItem("checkout")}`
+    }
+    else {
+        alert("A data de check-out deve ser posterior a de check-in")
+        checkout.value = ''
+    }
+
+    
 }
 // Guarda no localstorage o valor da quantidade de pessoas sempre que for mudado
 qtdPessoas.onchange = function () {
@@ -73,13 +99,13 @@ qtdPessoas.onchange = function () {
 
     switch (document.querySelector("#bookingApt").innerText) {
         case "Classic":
-            document.querySelector("#bookingValor").innerText = `R$ ${(quartos[1]["valor"] * qtdAtual).toFixed(2)}`
+            document.querySelector("#bookingValor_modal").innerText = `R$ ${(quartos[1]["valor"] * qtdAtual).toFixed(2)}`
             break
         case "Premium":
-            document.querySelector("#bookingValor").innerText = `R$ ${(quartos[2]["valor"] * qtdAtual).toFixed(2)}`
+            document.querySelector("#bookingValor_modal").innerText = `R$ ${(quartos[2]["valor"] * qtdAtual).toFixed(2)}`
             break
         default:
-            document.querySelector("#bookingValor").innerText = `R$ ${(quartos[0]["valor"] * qtdAtual).toFixed(2)}`
+            document.querySelector("#bookingValor_modal").innerText = `R$ ${(quartos[0]["valor"] * qtdAtual).toFixed(2)}`
             break
     }
 }
@@ -93,20 +119,62 @@ $("input[name='roomChoice']").click(function() {
             case "classic":
                 localStorage.setItem("quarto", quartos[1]['nome'])
                 document.querySelector("#bookingApt").innerText = `Classic`
-                document.querySelector("#bookingValor").innerText = `R$ ${(quartos[1]["valor"] * qtdAtual).toFixed(2)}`
+                ddocument.querySelector("#bookingValor_modal").innerText = `R$ ${(quartos[1]["valor"] * qtdAtual).toFixed(2)}`
                 break
             case "premium":
                 localStorage.setItem("quarto", quartos[2]["nome"])
                 document.querySelector("#bookingApt").innerText = `Premium`
-                document.querySelector("#bookingValor").innerText = `R$ ${(quartos[2]["valor"] * qtdAtual).toFixed(2)}`
+                document.querySelector("#bookingValor_modal").innerText = `R$ ${(quartos[2]["valor"] * qtdAtual).toFixed(2)}`
                 break
             default:
                 localStorage.setItem("quarto", quartos[0]["nome"])
                 document.querySelector("#bookingApt").innerText = `Executive`
-                document.querySelector("#bookingValor").innerText = `R$ ${(quartos[0]["valor"] * qtdAtual).toFixed(2)}`
+                document.querySelector("#bookingValor_modal").innerText = `R$ ${(quartos[0]["valor"] * qtdAtual).toFixed(2)}`
                 break
         }
     }
 
 })
 
+// Verificar quando e qual opção de quarto foi selecionada 
+$("#continuar").click(function() {
+
+    if (
+        localStorage.getItem("quarto") &&
+        localStorage.getItem("checkin") &&
+        localStorage.getItem("checkout") &&
+        localStorage.getItem("qtdPessoas")
+    ) {
+        
+        $(".modal").modal("show")
+
+        document.querySelector("#bookingApt_modal").innerText = localStorage.getItem("quarto")[0].toUpperCase() + localStorage.getItem("quarto").substr(1)
+        document.querySelector("#bookingCheckIn_modal").innerText = localStorage.getItem("checkin")
+        document.querySelector("#bookingCheckOut_modal").innerText = localStorage.getItem("checkout")
+        document.querySelector("#bookingQtd_modal").innerText = localStorage.getItem("qtdPessoas")
+
+        switch (localStorage.getItem("quarto")) {
+            case "classic":
+                document.querySelector("#modal-quarto p:nth-of-type(1)").innerText = quartos[1]["descricao"]
+                document.querySelector("#modal-quarto p:nth-of-type(2)").innerText = `R$ ${(quartos[1]["valor"]).toFixed(2)}`
+                document.querySelector("#bookingImg_modal").src = '/images/acomodacoes/acomodacoes-luxo-1.png'
+                document.querySelector("#bookingValor_modal").innerText = `R$ ${(quartos[1]["valor"] * qtdAtual).toFixed(2)}`
+                break
+            case "premium":
+                document.querySelector("#modal-quarto p:nth-of-type(1)").innerText = quartos[2]["descricao"]
+                document.querySelector("#modal-quarto p:nth-of-type(2)").innerText = `R$ ${(quartos[2]["valor"]).toFixed(2)}`
+                document.querySelector("#bookingImg_modal").src = '/images/acomodacoes/acomodacoes-standard-1.png'
+                document.querySelector("#bookingValor_modal").innerText = `R$ ${(quartos[2]["valor"] * qtdAtual).toFixed(2)}`
+                break
+            default:
+                document.querySelector("#modal-quarto p:nth-of-type(1)").innerText = quartos[0]["descricao"]
+                document.querySelector("#modal-quarto p:nth-of-type(2)").innerText = `R$ ${(quartos[0]["valor"]).toFixed(2)}`
+                document.querySelector("#bookingImg_modal").src = '/images/acomodacoes/acomodacoes-presidencial-1.png'
+                document.querySelector("#bookingValor_modal").innerText = `R$ ${(quartos[0]["valor"] * qtdAtual).toFixed(2)}`
+                break
+        }
+    }
+    else {
+        alert ("Por favor, preencha todos os campos!")
+    }
+})
